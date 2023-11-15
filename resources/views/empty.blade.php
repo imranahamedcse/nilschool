4dc93e07476b0e044a13bc171e89f111c6de8fc1
imranahamedcse <imranahamed.cse@gmail.com>
2023-11-15 16:57:15 +0600

    project install and mastering

---
+++ resources/views/empty.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+
+@section('title')
+    empty
+@endsection
+
+@section('content')
+    <h1>empty</h1>
+@endsection
+
+@push('script')
+    <!-- script -->
+@endpush
+
+@push('style')
+    <!-- style -->
+@endpush
